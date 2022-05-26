d9c2aad1d6a629978e399fc8334b15822d8b73db
tushar <tushar.singhal,mec19@itbhu.ac.in>
2022-05-26 21:41:12 +0530

    static file changes

--- eng_backend/settings.py
+++ eng_backend/settings.py
@@ -188,6 +188,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 django_heroku.settings(locals())
